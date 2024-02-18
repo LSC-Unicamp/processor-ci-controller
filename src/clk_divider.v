10a96ba8a462334747e426b22cbeef42f4e52a90
Julio Nunes Avelar <jn5.guape@gmail.com>
2024-02-18 12:23:36 -0300

    Implementando logica de contador de pulsos e criando testes
    Criando logica para executar uma quantidade especifica de pulsos de clk,
    e criando teste de simulação para o modulo

--- src/clk_divider.v
+++ src/clk_divider.v
@@ -1,51 +1,67 @@
 module ClkDivider #(
-    parameter COUNTER_BITS = 32
+    parameter COUNTER_BITS = 32,
+    parameter PULSE_BITS = 32
 )(
     input wire clk,
     input wire reset,
+    input wire write_pulse,
     input wire option, // 0 - pulse, 1 - auto
     input wire out_enable, // 0 not, 1 - yes
     input wire [COUNTER_BITS - 1:0] divider,
-    input wire pulse,
+    input wire [PULSE_BITS - 1:0] pulse,
     output wire clk_o
 );
 
-reg clk_o_pulse, clk_o_auto;
+wire clk_o_pulse;
+reg clk_o_auto;
 reg [COUNTER_BITS - 1:0] clk_counter;
+reg [PULSE_BITS - 1:0] pulse_counter;
 
 assign clk_o = (out_enable == 1'b1) ? (option == 1'b0) ? 
     clk_o_pulse : clk_o_auto : 1'b0;
 
+assign clk_o_pulse = (pulse_counter != 32'd0) ? clk : 1'b0;
+
 initial begin
-    clk_counter = 0;
-    clk_o_pulse = 0;
-    clk_o_auto = 0;
+    clk_counter = 32'd0;
+    clk_o_auto = 1'b0;
+    pulse_counter = 32'd0;
 end
 
 always @(posedge clk ) begin
     if(reset == 1'b1) begin
-        clk_counter <= 0;
-        clk_o_auto <= 0;
+        clk_counter <= 32'd0;
+        clk_o_auto <= 32'd0;
     end else begin
         if(clk_counter == 0) begin
             clk_o_auto <= 1'b1;
-            clk_counter <= clk_counter + 1;
+            clk_counter <= clk_counter + 1'b1;
         end else if(clk_counter == divider /2) begin 
             clk_o_auto <= 1'b0;
-            clk_counter <= clk_counter + 1;
+            clk_counter <= clk_counter + 1'b1;
         end else begin
-            clk_counter <= clk_counter + 1;
+            clk_counter <= clk_counter + 1'b1;
         end
 
         if(clk_counter >= divider - 1) begin
-            clk_counter <= 0;
+            clk_counter <= 32'd0;
         end
     end
 end
 
 always @(posedge clk ) begin
-    if(pulse == 1'b1) begin
-        
+    if(reset == 1'b1) begin
+        pulse_counter <= 32'd0;
+    end else begin
+        if(write_pulse == 1'b1) begin
+            pulse_counter <= pulse;
+        end
+
+        if(pulse_counter > 32'd0) begin
+            pulse_counter <= pulse_counter - 1'b1;
+        end else begin
+            pulse_counter <= 32'd0;
+        end 
     end
 end
 
