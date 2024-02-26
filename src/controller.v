b62c30a3e01c6a29ae8eb923daa6722d59957d6c
Viktor-Lake <victorprudentelago@outlook.com>
2024-02-26 17:30:52 -0300

    Correções finais

--- src/controller.v
+++ src/controller.v
@@ -10,42 +10,42 @@ module Controller #(
 
     output wire reset_core,
 
-    output wire memory_read_memory,
-    output wire memory_write_memory,
-    output wire [31:0] address_memory,
-    output wire [31:0] write_data_memory,
-    input wire [31:0]read_data_memory,
+    input wire memory_read_memory,
+    input wire memory_write_memory,
+    input wire [31:0] address_memory,
+    input wire [31:0] write_data_memory,
+    output wire [31:0]read_data_memory,
 
     //RISC-V FORMAL INTERFACE(RVFI)
     
     //INSTRUCTION METADATA (check: https://github.com/YosysHQ/riscv-formal/blob/main/cores/nerv/nerv.sv)
-    input reg        rvfi_valid,
-    input reg [63:0] rvfi_order,
-    input reg [31:0] rvfi_insn,
-    input reg        rvfi_trap,
-    input reg        rvfi_halt,
-    input reg        rvfi_intr,
-    input reg [ 1:0] rvfi_mode,
-    input reg [ 1:0] rvfi_ixl,
+    input wire        rvfi_valid,
+    input wire [63:0] rvfi_order,
+    input wire [31:0] rvfi_insn,
+    input wire        rvfi_trap,
+    input wire        rvfi_halt,
+    input wire        rvfi_intr,
+    input wire [ 1:0] rvfi_mode,
+    input wire [ 1:0] rvfi_ixl,
     
-    //INTEGER REGISTER READ/WRITE
-    input reg [ 4:0] rvfi_rs1_addr,
-    input reg [ 4:0] rvfi_rs2_addr,
-    input reg [31:0] rvfi_rs1_rdata,
-    input reg [31:0] rvfi_rs2_rdata,
-    input reg [ 4:0] rvfi_rd_addr,
-    input reg [31:0] rvfi_rd_wdata,
+    //INTEGER wireISTER READ/WRITE
+    input wire [ 4:0] rvfi_rs1_addr,
+    input wire [ 4:0] rvfi_rs2_addr,
+    input wire [31:0] rvfi_rs1_rdata,
+    input wire [31:0] rvfi_rs2_rdata,
+    input wire [ 4:0] rvfi_rd_addr,
+    input wire [31:0] rvfi_rd_wdata,
 
     //PROGRAM COUNTER
-    input reg [31:0] rvfi_pc_rdata,
-    input reg [31:0] rvfi_pc_wdata,
+    input wire [31:0] rvfi_pc_rdata,
+    input wire [31:0] rvfi_pc_wdata,
 
     //MEMORY ACCESS
-    input [31:0] rvfi_mem_addr,
-    input [ 3:0] rvfi_mem_rmask,
-    input [ 3:0] rvfi_mem_wmask,
-    input [31:0] rvfi_mem_rdata,
-    input [31:0] rvfi_mem_wd
+    input wire [31:0] rvfi_mem_addr,
+    input wire [ 3:0] rvfi_mem_rmask,
+    input wire [ 3:0] rvfi_mem_wmask,
+    input wire [31:0] rvfi_mem_rdata,
+    input wire [31:0] rvfi_mem_wd
 );
     
 endmodule
