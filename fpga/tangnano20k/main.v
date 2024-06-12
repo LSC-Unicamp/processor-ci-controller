baf9a3f20318fcc3bb4881eb08fb5b144a259fb3
Julio Nunes Avelar <jn5.guape@gmail.com>
2024-06-11 22:30:26 -0300

    Padronizando cascas das FPGAs
    Padronizando IOs e reports para todas as FPGAs

--- fpga/tangnano20k/main.v
+++ fpga/tangnano20k/main.v
@@ -1,18 +1,10 @@
 module top (
-    input wire clk,
-    input wire reset,
-    input wire rx,
+    input  wire clk, // 27 mhz
+    input  wire reset,
+    input  wire rx,
     output wire tx,
     output wire [7:0]led,
-    inout [5:0]gpios
-);
-
-Controller #(
-    .CLK_FREQ(27000000)
-) Controller(
-    .clk(clk),
-    .tx(tx),
-    .rx(rx)
+    input  wire [5:0]gpios
 );
     
 endmodule
--- fpga/tangnano20k/run.tcl
+++ fpga/tangnano20k/run.tcl
@@ -2,6 +2,15 @@ set_device -name GW2AR-18C GW2AR-LV18QN88C8/I7
 add_file fpga/tangnano20k/pinout.cst
 add_file fpga/tangnano20k/top.sdc
 add_file fpga/tangnano20k/main.v
+add_file ../src/uart.v
+add_file ../src/uart_rx.v
+add_file ../src/uart_tx.v
+add_file ../src/fifo.v
+add_file ../src/reset.v
+add_file ../src/clk_divider.v
+add_file ../src/memory.v
+add_file ../src/interpreter.v
+add_file ../src/controller.v
 
 set_option -use_mspi_as_gpio 1
 set_option -use_sspi_as_gpio 1
