26cea035d3966a5456fd6db001a3bee8f627c574
Julio Nunes Avelar <jn5.guape@gmail.com>
2025-04-06 20:57:31 -0300

    Adicionando novo exemplo

---
+++ examples/Grande-Risco-5.sv
@@ -0,0 +1,103 @@
+module top (
+    input  logic clk,
+    input  logic CPU_RESETN,
+
+    input  logic rx,
+    output logic tx,
+
+    input  logic mosi,
+    output logic miso,
+    input  logic sck,
+    input  logic cs
+);
+
+logic clk_o;
+logic clk_core, rst_core;
+
+// Fios do barramento entre Controller e Processor
+logic core_cyc;
+logic core_stb;
+logic core_we;
+logic [31:0] core_addr;
+logic [31:0] core_data_out;
+logic [31:0] core_data_in;
+logic        core_ack;
+
+Controller #(
+    .CLK_FREQ           (50000000),
+    .BIT_RATE           (BIT_RATE),
+    .PAYLOAD_BITS       (8),
+    .BUFFER_SIZE        (8),
+    .PULSE_CONTROL_BITS (32),
+    .BUS_WIDTH          (32),
+    .WORD_SIZE_BY       (4),
+    .ID                 (32'h7700006A),
+    .RESET_CLK_CYCLES   (20),
+    .MEMORY_FILE        (""),
+    .MEMORY_SIZE        (4096)
+) u_Controller (
+    .clk                (clk),
+    .rst_n              (CPU_RESETN),
+    
+    // SPI signals
+    .sck_i              (sck),
+    .cs_i               (cs),
+    .mosi_i             (mosi),
+    .miso_o             (miso),
+    
+    // SPI callback signals
+    .rw_i               (),
+    .intr_o             (),
+    
+    // UART signals
+    .rx                 (rx),
+    .tx                 (tx),
+    
+    // Clock, reset, and bus signals
+    .clk_core_o         (clk_core),
+    .rst_core_o         (rst_core),
+    
+    // Barramento padrão (não AXI4-Lite)
+    .core_cyc_i         (core_cyc),
+    .core_stb_i         (core_stb),
+    .core_we_i          (core_we),
+    .core_addr_i        (core_addr),
+    .core_data_i        (core_data_out),
+    .core_data_o        (core_data_in),
+    .core_ack_o         (core_ack)
+);
+
+Grande_Risco5 #(
+    .BOOT_ADDRESS           (32'h00000000),
+    .I_CACHE_SIZE           (256),
+    .D_CACHE_SIZE           (256),
+    .DATA_WIDTH             (32),
+    .ADDR_WIDTH             (32),
+    .BRANCH_PREDICTION_SIZE (128)
+) Processor (
+    .clk    (clk_core),
+    .rst_n  (~rst_core),
+    .halt   (1'b0),
+
+    .cyc_o  (core_cyc),
+    .stb_o  (core_stb),
+    .we_o   (core_we),
+
+    .addr_o (core_addr),
+    .data_o (core_data_out),
+
+    .ack_i  (core_ack),
+    .data_i (core_data_in),
+
+    .interruption (1'b0)
+);
+
+always_ff @(posedge clk) begin : CLOCK_DIVIDER
+    if (!CPU_RESETN) begin
+        clk_o <= 1'b0;
+    end else begin
+        clk_o <= ~clk_o;
+    end
+end
+    
+endmodule
